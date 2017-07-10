cd75e56f9bfa94687f2aa554a515f7b92b6057e7
DianaCM11 <diana.cm08@gmail.com>
2017-07-09 19:58:44 -0600

    Consulta Base de Datos

--- Backend/SistemaGestionEstudiantil.py
+++ Backend/SistemaGestionEstudiantil.py
@@ -4,9 +4,17 @@ import cx_Oracle
 app = Flask(__name__)
 
 #Conexión a base de datos (Oracle Express Edition 11g)
-con = cx_Oracle.connect('TCU/oracle@127.0.0.1') #usuario/contraseña
-print (con.version)
-con.close()
+TNS = cx_Oracle.makedsn('localhost',1521,'XE')
+laBaseDeDatos = cx_Oracle.connect('TCU','oracle',TNS) #usuario/contraseña
+elCursor = laBaseDeDatos.cursor()
+
+elCursor.execute('SELECT * FROM ESTUDIANTE')
+for registro in elCursor:
+    print (registro)
+
+@app.route('/')
+def hello_world():
+   return 'Hola Mundo'
 
 if __name__ == '__main__':
     app.run()
