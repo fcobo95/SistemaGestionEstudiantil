a2ace3ec684ce8e33cc6e10d2ceba35d5b0cd49c
DianaCM11 <diana.cm08@gmail.com>
2017-07-09 18:02:02 -0600

    Conexión Base de Datos

--- Backend/SistemaGestionEstudiantil.py
+++ Backend/SistemaGestionEstudiantil.py
@@ -1,12 +1,12 @@
 from flask import Flask
+import cx_Oracle
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+#Conexión a base de datos (Oracle Express Edition 11g)
+con = cx_Oracle.connect('TCU/oracle@127.0.0.1') #usuario/contraseña
+print (con.version)
+con.close()
 
 if __name__ == '__main__':
     app.run()
