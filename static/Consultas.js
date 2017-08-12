37f6676663527e206bc127fe963b81006f333e20
Joshua <jcamposc889@ulacit.ed.cr>
2017-08-12 15:17:25 -0600

    Manejo de errores en selección de niveles en vista de consultas.

--- static/Consultas.js
+++ static/Consultas.js
@@ -56,37 +56,42 @@ function modificarSeccion(){
 
     var elMenuDeNiveles = document.getElementById("nivel");
     var elNivel = elMenuDeNiveles.options[elMenuDeNiveles.selectedIndex].text;
-
     var elMenuDeSecciones= document.getElementById("seccion");
-    $('#seccion').empty();
-    var elementoVacio = document.createElement("option");
-    elementoVacio.text = "";
-    elMenuDeSecciones.add(elementoVacio);
 
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "http://127.0.0.1:5000/obtengaSecciones",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "cache-control": "no-cache",
-            "postman-token": "1daca2df-ebcd-1314-f917-4f39bd27a675"
-        },
-        "processData": false,
-        "data": JSON.stringify({"nivel": elNivel})
-}
+    if (elNivel == ""){
+        $("#errorNivel").show()
+        $('#seccion').empty();
+        var elementoVacio = document.createElement("option");
+        elementoVacio.text = "";
+        elMenuDeSecciones.add(elementoVacio);
+    } else {
+        $('#seccion').empty();
+        var elementoVacio = document.createElement("option");
+        elementoVacio.text = "";
+        elMenuDeSecciones.add(elementoVacio);
 
-$.ajax(settings).done(function (response) {
-  lasSecciones = response['secciones'];
-  laCantidadDeSecciones = lasSecciones.length;
-  elContador = 0
-  while (elContador < laCantidadDeSecciones){
-    var nuevoElemento = document.createElement("option");
-    nuevoElemento.text = lasSecciones[elContador];
-    elMenuDeSecciones.add(nuevoElemento);
-    elContador += 1
-  }
-});
-
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://127.0.0.1:5000/obtengaSecciones",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify({"nivel": elNivel})
+    }
+        $.ajax(settings).done(function (response) {
+            lasSecciones = response['secciones'];
+            laCantidadDeSecciones = lasSecciones.length;
+            elContador = 0
+            while (elContador < laCantidadDeSecciones){
+                var nuevoElemento = document.createElement("option");
+                nuevoElemento.text = lasSecciones[elContador];
+                elMenuDeSecciones.add(nuevoElemento);
+                elContador += 1
+            }
+        });
+    }
 }
