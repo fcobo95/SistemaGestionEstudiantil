8e96f6941acfa2f139ba593f618559f976b2c50d
Joshua <jcamposc889@ulacit.ed.cr>
2017-08-12 16:56:47 -0600

    Método para obtener y mostrar estudiantes según sección en frontend.

--- static/Consultas.js
+++ static/Consultas.js
@@ -95,3 +95,62 @@ function modificarSeccion(){
         });
     }
 }
+
+function modificarEstudiante(){
+
+    $(".alert").hide()
+
+    var elMenuDeSecciones = document.getElementById("seccion");
+    var laSeccion = elMenuDeSecciones.options[elMenuDeSecciones.selectedIndex].text;
+    var elMenuDeEstudiantes= document.getElementById("estudiante");
+
+    if (laSeccion == ""){
+        $("#errorSeccion").show()
+        $('#estudiante').empty();
+        var elementoVacio = document.createElement("option");
+        elementoVacio.text = "";
+        elMenuDeEstudiantes.add(elementoVacio);
+    } else {
+        $('#estudiante').empty();
+        var elementoVacio = document.createElement("option");
+        elementoVacio.text = "";
+        elMenuDeEstudiantes.add(elementoVacio);
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://127.0.0.1:5000/obtengaEstudiantes",
+            "method": "POST",
+            "headers": {
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "c74b583f-d742-4792-0dc8-29732420b2bf"
+        },
+        "processData": false,
+        "data": JSON.stringify({"seccion":laSeccion})
+        }
+
+        $.ajax(settings).done(function (response) {
+
+            sessionStorage.clear();
+            laCantidadDeEstudiantes = Object.keys(response).length;
+            elContador = 1;
+
+            while (elContador < laCantidadDeEstudiantes + 1) {
+                laIdentificacion = response[elContador.toString()]["identificacion"];
+                elNombre = response[elContador.toString()]["nombre"];
+                elApellido1 = response[elContador.toString()]["apellido1"];
+                elApellido2 = response[elContador.toString()]["apellido2"];
+                elNombreCompleto = elNombre + " " + elApellido1 + " " + elApellido2
+
+                var nuevoElemento = document.createElement("option");
+                nuevoElemento.text = elNombreCompleto;
+                elMenuDeEstudiantes.add(nuevoElemento);
+
+                sessionStorage.setItem(elContador, laIdentificacion);
+
+                elContador+=1
+            }
+        });
+    }
+}
