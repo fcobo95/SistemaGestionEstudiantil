1a8b62e198de214aeef2ca32a00837c8499376d8
Joshua <jcamposc889@ulacit.ed.cr>
2017-08-12 15:01:35 -0600

    Función para obtener secciones y mostrar resultados en frontend.

--- static/Consultas.js
+++ static/Consultas.js
@@ -7,6 +7,9 @@ function modificarNivel(){
 
     var elMenuDeNiveles= document.getElementById("nivel");
     $('#nivel').empty();
+    var elementoVacio = document.createElement("option");
+    elementoVacio.text = "";
+    elMenuDeNiveles.add(elementoVacio);
 
     switch (elCiclo)
     {
@@ -15,7 +18,6 @@ function modificarNivel(){
             var nuevoElemento = document.createElement("option");
             nuevoElemento.text = elNivel.toString();
             elMenuDeNiveles.add(nuevoElemento);
-            console.log(nuevoElemento.text);
         }
         break;
 
@@ -24,7 +26,6 @@ function modificarNivel(){
             var nuevoElemento = document.createElement("option");
             nuevoElemento.text = elNivel.toString();
             elMenuDeNiveles.add(nuevoElemento);
-            console.log(nuevoElemento.text);
         }
         break;
 
@@ -33,7 +34,6 @@ function modificarNivel(){
             var nuevoElemento = document.createElement("option");
             nuevoElemento.text = elNivel.toString();
             elMenuDeNiveles.add(nuevoElemento);
-            console.log(nuevoElemento.text);
         }
         break;
 
@@ -42,13 +42,51 @@ function modificarNivel(){
             var nuevoElemento = document.createElement("option");
             nuevoElemento.text = elNivel.toString();
             elMenuDeNiveles.add(nuevoElemento);
-            console.log(nuevoElemento.text);
         }
         break;
 
         default:
             $("#errorCiclo").show()
-            //$("#result").addClass("alert alert-warning").text("Seleccione un ciclo.");
-            //$("#result").alert();
     }
+}
+
+function modificarSeccion(){
+
+    $(".alert").hide()
+
+    var elMenuDeNiveles = document.getElementById("nivel");
+    var elNivel = elMenuDeNiveles.options[elMenuDeNiveles.selectedIndex].text;
+
+    var elMenuDeSecciones= document.getElementById("seccion");
+    $('#seccion').empty();
+    var elementoVacio = document.createElement("option");
+    elementoVacio.text = "";
+    elMenuDeSecciones.add(elementoVacio);
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://127.0.0.1:5000/obtengaSecciones",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "1daca2df-ebcd-1314-f917-4f39bd27a675"
+        },
+        "processData": false,
+        "data": JSON.stringify({"nivel": elNivel})
+}
+
+$.ajax(settings).done(function (response) {
+  lasSecciones = response['secciones'];
+  laCantidadDeSecciones = lasSecciones.length;
+  elContador = 0
+  while (elContador < laCantidadDeSecciones){
+    var nuevoElemento = document.createElement("option");
+    nuevoElemento.text = lasSecciones[elContador];
+    elMenuDeSecciones.add(nuevoElemento);
+    elContador += 1
+  }
+});
+
 }
